8350d50bb4a45f1fa5a7a0cde0572aa227a15eb7
Peter <undevil89@gmail.com>
2022-10-03 19:04:51 +0300

    Third commit

--- src/Demo.java
+++ src/Demo.java
@@ -2,5 +2,7 @@ public class Demo {
     public static void main(String[] args) {
         System.out.println("KEK");
         System.out.println("KEK2");
+        System.out.println("KEK3");
+
     }
 }
